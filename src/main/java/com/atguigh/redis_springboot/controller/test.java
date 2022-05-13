ade4b9d8652e122413419c905fd2f5060a4b4166
zxj <1459611132@qq.com>
2022-05-13 23:27:20 +0800

    test1

--- src/main/java/com/atguigh/redis_springboot/controller/test.java
+++ src/main/java/com/atguigh/redis_springboot/controller/test.java
@@ -3,6 +3,6 @@ package com.atguigh.redis_springboot.controller;
 public class test {
     public static void main(String[] args) {
         System.out.println("111");
-        System.out.println("222");
+        System.out.println("333");
     }
 }
